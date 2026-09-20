02e2156a1086e08a93438093bc44b15225842d1d
Lambert Florian <lambert.f@lfinfo.be>
2026-09-20 14:23:21 +0200

    migration fix

--- src/migrations/1789158650755-licenserenewal.ts
+++ src/migrations/1789158650755-licenserenewal.ts
@@ -3,9 +3,9 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class Licenserenewal1789158650755 implements MigrationInterface {
 
    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN "renewalReminder15SentAt"`, undefined);
-        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN "renewalReminder7SentAt"`, undefined);
-        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN "renewalReminder1SentAt"`, undefined);
+        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN IF EXISTS "renewalReminder15SentAt"`, undefined);
+        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN IF EXISTS "renewalReminder7SentAt"`, undefined);
+        await queryRunner.query(`ALTER TABLE "order_license" DROP COLUMN IF EXISTS "renewalReminder1SentAt"`, undefined);
         await queryRunner.query(`ALTER TABLE "order_license" ADD "renewalReminder10SentAt" TIMESTAMP WITH TIME ZONE`, undefined);
         await queryRunner.query(`ALTER TABLE "order_license" ADD "renewalReminder3SentAt" TIMESTAMP WITH TIME ZONE`, undefined);
    }
